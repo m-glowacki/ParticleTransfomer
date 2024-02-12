628f79af31e7ddd000facee77a41e99cea426878
Maciej Mikolaj Glowacki <maciej.mikolaj.glowacki@cern.ch>
2024-02-12 14:04:39 +0000

    changes to cls_token imp

--- particle_transformer.py
+++ particle_transformer.py
@@ -4,13 +4,15 @@ import torch.nn.functional as F
 
 
 class Embed(nn.Module):
-    def __init__(self, input_dim, output_dim, normalize_input=True, event_level=False, activation='gelu'):
+    def __init__(self, input_dim, output_dim, normalize_input=False, event_level=False, activation='gelu'):
         super().__init__()
 
         self.input_bn = nn.BatchNorm1d(input_dim) if normalize_input else None
-        self.linear = nn.Linear(input_dim, output_dim)
-        self.activation = F.gelu
+        self.fc1 = nn.Linear(input_dim, output_dim)
+        self.fc2 = nn.Linear(output_dim, output_dim)
+        self.fc3 = nn.Linear(output_dim, output_dim)
         self.event_level = event_level
+        
 
     def forward(self, x):
         if self.input_bn is not None:
@@ -19,12 +21,12 @@ class Embed(nn.Module):
             if not self.event_level: 
                 x = x.permute(2, 0, 1).contiguous()
 
-        # x: (seq_len, batch, embed_dim)
-        x = self.linear(x)
-        # Apply activation function
-        x = self.activation(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
 
         return x
+    
 
 class AttBlock(nn.Module):
     def __init__(self, embed_dims, linear_dims1, linear_dims2, num_heads=8, activation='relu'):
@@ -47,6 +49,7 @@ class AttBlock(nn.Module):
         # convert it to a boolean tensor with `True` for padded locations.
             padding_mask = padding_mask.bool()
 
+        
         x_att, attention = self.multihead_attention(x, x, x, key_padding_mask=padding_mask, need_weights=True, average_attn_weights=True)
         
         # Skip connection
@@ -78,9 +81,8 @@ class ClassBlock(nn.Module):
         self.linear2 = nn.Linear(linear_dims1, linear_dims2)
 
     def forward(self, x, class_token, padding_mask=None):
-        # Concatenate the class token to the input sequence along the sequence length dimension
+        # Concatenate the class token to the input sequence along the sequence length dimension  
         x = torch.cat((class_token, x), dim=0)  # (seq_len+1, batch, embed_dim)
-
         # Layer normalization 1
         x = self.layer_norm1(x)
 
@@ -123,6 +125,8 @@ class AnalysisObjectTransformer(nn.Module):
     def __init__(self, input_dim_obj, input_dim_event, embed_dims, linear_dims1, linear_dims2, mlp_hidden_1, mlp_hidden_2, num_heads=8):
         super(AnalysisObjectTransformer, self).__init__()
 
+        self.embed_dims = embed_dims
+       
         # Embedding layer (assumed to be external)
         self.embedding_layer = Embed(input_dim_obj, embed_dims)
         self.embedding_layer_event_level = Embed(input_dim_event, embed_dims, event_level=True)
@@ -137,19 +141,23 @@ class AnalysisObjectTransformer(nn.Module):
 
         # Output linear layer and sigmoid activation
 
-        self.mlp = MLPHead(embed_dims *2, mlp_hidden_1, mlp_hidden_2, output_dim=1)
+        self.mlp = MLPHead(embed_dims + input_dim_event, mlp_hidden_1, mlp_hidden_2, output_dim=1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x, event_level, mask=None):
 
-        cls_token = nn.Parameter(torch.zeros(1, 1, 64), requires_grad=True)
+        cls_token = nn.Parameter(torch.zeros(1, 1, self.embed_dims), requires_grad=True)
+        cls_token = nn.init.trunc_normal_(cls_token, std=.02)
         # Embedding layer
-        x = self.embedding_layer(x)
 
+        x = self.embedding_layer(x)
+        x = x.permute(1, 0, 2)
+        
         attention_weights = []
 
         # Three blocks of self-attention
         x, attention = self.block1(x, padding_mask=mask)
+
         attention_weights.append(attention)
         x, attention  = self.block2(x, padding_mask=mask)
         attention_weights.append(attention)
@@ -161,12 +169,7 @@ class AnalysisObjectTransformer(nn.Module):
         cls_tokens, attention  = self.block6(x, cls_tokens, padding_mask=mask)
         cls_tokens, attention  = self.block7(x, cls_tokens, padding_mask=mask)
 
-        # Global average pooling (assuming sequence length is the first dimension)
-        x = x.mean(dim=0)
-
-        event_level = self.embedding_layer_event_level(event_level)
-
-        x = torch.cat((x, event_level), dim=1)
+        x = torch.cat((cls_tokens.squeeze(0), event_level), dim=-1)
         x = self.mlp(x)
         output_probabilities = self.sigmoid(x)
         return output_probabilities, attention_weights
